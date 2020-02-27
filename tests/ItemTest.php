299fa33030eafa083323976cd9dae82f75d37a3c
jeyroik <jeyroik@gmail.com>
2020-02-27 18:26:00 +0300

    tests

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -5,7 +5,6 @@ use extas\interfaces\repositories\IRepository;
 use extas\components\plugins\Plugin;
 use extas\components\SystemContainer;
 use extas\components\Item;
-use extas\interfaces\plugins\IPluginRepository;
 use extas\interfaces\stages\IStageRepository;
 use extas\components\stages\Stage;
 use extas\interfaces\stages\IStage;
@@ -17,9 +16,13 @@ use extas\interfaces\stages\IStage;
 class ItemTest extends TestCase
 {
     /**
-     * @var IRepository|mixed|null
+     * @var IRepository|null
      */
     protected ?IRepository $pluginRepo = null;
+
+    /**
+     * @var IRepository|null
+     */
     protected ?IRepository $stageRepo = null;
 
     protected function setUp(): void
@@ -28,6 +31,10 @@ class ItemTest extends TestCase
         $env = \Dotenv\Dotenv::create(getcwd() . '/tests/');
         $env->load();
 
+        /**
+         * For faster operations PluginRepository caches plugins->stage map in memory.
+         * But we are creating new plugins runtime, so we need to have possibility to reload memory cache.
+         */
         $this->pluginRepo = new class extends \extas\components\plugins\PluginRepository {
             public function reload()
             {
@@ -37,18 +44,15 @@ class ItemTest extends TestCase
         $this->stageRepo = SystemContainer::getItem(IStageRepository::class);
     }
 
-    public static function tearDownAfterClass(): void
-    {
-        $repo = SystemContainer::getItem(IPluginRepository::class);
-        $repo->delete([Plugin::FIELD__CLASS => 'NotExistingClass']);
-
-        $repo = SystemContainer::getItem(IStageRepository::class);
-        $repo->delete([IStage::FIELD__HAS_PLUGINS => true]);
-    }
-
     /**
-     * Test default item configuration.
+     * Clean up
      */
+    public function tearDown(): void
+    {
+        $this->pluginRepo->delete([Plugin::FIELD__CLASS => 'NotExistingClass']);
+        $this->stageRepo->delete([IStage::FIELD__HAS_PLUGINS => true]);
+    }
+    
     public function testCorrectAppliesConfigOnConstruct(): void
     {
         $must = [
@@ -75,24 +79,65 @@ class ItemTest extends TestCase
         $this->assertEquals($must, $child->__toArray());
     }
 
-    public function testRecallsOnStageInit()
+    public function testStageEntityInitIsRising()
     {
-        $this->createPluginAndStage('init');
+        $this->createPluginAndStage('init', 'Init');
         $this->pluginRepo->reload();
-        $this->expectExceptionMessage('Class \'NotExistingClass\' not found');
+        $this->expectExceptionMessage('Class \'NotExistingClassInit\' not found');
+        new class extends Item {
+            protected function getSubjectForExtension(): string
+            {
+                return 'test.child';
+            }
+        };
+    }
+
+    public function testStageEntityInitIsNotRising()
+    {
+        $this->createPluginAndStage('init', 'Init');
+        $this->pluginRepo->reload();
+        new class extends Item {
+            protected bool $isAllowInitStage = false;
+            protected function getSubjectForExtension(): string
+            {
+                return 'test.child';
+            }
+        };
+    }
+
+    public function testStageEntityAfterIsRising()
+    {
+        $this->createPluginAndStage('after', 'After');
+        $this->pluginRepo->reload();
         $child = new class extends Item {
             protected function getSubjectForExtension(): string
             {
                 return 'test.child';
             }
         };
+        $this->expectExceptionMessage('Class \'NotExistingClassAfter\' not found');
+        unset($child);
     }
 
-    public function testRecallsOnStageAfter()
+    public function testStageEntityAfterIsNotRising()
     {
-        $this->createPluginAndStage('after');
+        $this->createPluginAndStage('after', 'After');
         $this->pluginRepo->reload();
-        $this->expectExceptionMessage('Class \'NotExistingClass\' not found');
+        $child = new class extends Item {
+            protected bool $isAllowAfterStage = false;
+            protected function getSubjectForExtension(): string
+            {
+                return 'test.child';
+            }
+        };
+        unset($child);
+    }
+
+    public function testStageEntityToArrayIsRising()
+    {
+        $this->createPluginAndStage('to.array', 'ToArray');
+        $this->pluginRepo->reload();
+        $this->expectExceptionMessage('Class \'NotExistingClassToArray\' not found');
         new class extends Item {
             protected function getSubjectForExtension(): string
             {
@@ -101,11 +146,24 @@ class ItemTest extends TestCase
         };
     }
 
-    public function testRecallsOnStageToArray()
+    public function testStageEntityToArrayIsNotRising()
     {
-        $this->createPluginAndStage('to.array');
+        $this->createPluginAndStage('to.array', 'ToArray');
         $this->pluginRepo->reload();
-        $this->expectExceptionMessage('Class \'NotExistingClass\' not found');
+        new class extends Item {
+            protected bool $isAllowToArrayStage = false;
+            protected function getSubjectForExtension(): string
+            {
+                return 'test.child';
+            }
+        };
+    }
+
+    public function testStageEntityToStringIsRising()
+    {
+        $this->createPluginAndStage('to.string', 'ToString');
+        $this->pluginRepo->reload();
+        $this->expectExceptionMessage('Class \'NotExistingClassToString\' not found');
         new class extends Item {
             protected function getSubjectForExtension(): string
             {
@@ -114,11 +172,24 @@ class ItemTest extends TestCase
         };
     }
 
-    public function testRecallsOnStageToString()
+    public function testStageEntityToStringIsNotRising()
     {
-        $this->createPluginAndStage('to.string');
+        $this->createPluginAndStage('to.string', 'ToString');
         $this->pluginRepo->reload();
-        $this->expectExceptionMessage('Class \'NotExistingClass\' not found');
+        new class extends Item {
+            protected bool $isAllowToStringStage = false;
+            protected function getSubjectForExtension(): string
+            {
+                return 'test.child';
+            }
+        };
+    }
+
+    public function testStageEntityToIntIsRising()
+    {
+        $this->createPluginAndStage('to.int', 'ToInt');
+        $this->pluginRepo->reload();
+        $this->expectExceptionMessage('Class \'NotExistingClassToInt\' not found');
         new class extends Item {
             protected function getSubjectForExtension(): string
             {
@@ -127,12 +198,12 @@ class ItemTest extends TestCase
         };
     }
 
-    public function testRecallsOnStageToInt()
+    public function testStageEntityToIntIsNotRising()
     {
-        $this->createPluginAndStage('to.int');
+        $this->createPluginAndStage('to.int', 'ToInt');
         $this->pluginRepo->reload();
-        $this->expectExceptionMessage('Class \'NotExistingClass\' not found');
         new class extends Item {
+            protected bool $isAllowToIntStage = false;
             protected function getSubjectForExtension(): string
             {
                 return 'test.child';
@@ -140,13 +211,20 @@ class ItemTest extends TestCase
         };
     }
 
-    protected function createPluginAndStage(string $stageSuffix)
+    /**
+     * Create plugin and stage records.
+     *
+     * @param string $stageSuffix
+     * @param string $pluginSuffix
+     */
+    protected function createPluginAndStage(string $stageSuffix, string $pluginSuffix)
     {
         $plugin = new Plugin([
-            Plugin::FIELD__CLASS => 'NotExistingClass',
+            Plugin::FIELD__CLASS => 'NotExistingClass' . $pluginSuffix,
             Plugin::FIELD__STAGE => 'test.child.' . $stageSuffix
         ]);
         $this->pluginRepo->create($plugin);
+
         $stage = new Stage([
             Stage::FIELD__NAME => 'test.child.' . $stageSuffix,
             Stage::FIELD__HAS_PLUGINS => true
