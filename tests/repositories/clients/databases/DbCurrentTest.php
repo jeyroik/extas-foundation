4aca3ac0014301f5b24a5dd5e62d948da1a0fff6
jeyroik <jeyroik@gmail.com>
2020-03-03 14:43:46 +0300

    added db current test

---
+++ tests/repositories/clients/databases/DbCurrentTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use \extas\components\repositories\clients\databases\DbCurrent;
+use \extas\interfaces\repositories\clients\IClientTable;
+
+/**
+ * Class DbCurrentTest
+ * 
+ * @author jeyroik <jeyroik@gmail.com>
+ */
+class DbCurrentTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $env = \Dotenv\Dotenv::create(getcwd() . '/tests/');
+        $env->load();
+    }
+
+    public function testGetTable()
+    {
+        $db = new class extends DbCurrent {
+            public static function getTables()
+            {
+                return static::$tables;
+            }
+        };
+
+        putenv('TEST_TABLE__NOT_EXISTING_REPO_NAME=tests');
+        putenv('TEST_DB__NOT_EXISTING_REPO_NAME=extas_tests');
+        putenv('TEST_DSN__NOT_EXISTING_REPO_NAME=mongodb://localhost:27017');
+        putenv('TEST_DRIVER__NOT_EXISTING_REPO_NAME=mongo');
+        $table = $db::getTable('not.existing.repo.name', 'test');
+        $this->assertInstanceOf(IClientTable::class, $table);
+        $must = [
+            'testnot.existing.repo.name' => $table
+        ];
+        $this->assertEquals($must, $db::getTables());
+    }
+
+    public function testGetTableException()
+    {
+        putenv('TEST_DRIVER__NOT_EXISTING_REPO=not.existing.driver');
+        $this->expectExceptionMessage('Unknown driver "not.existing.driver"');
+        DbCurrent::getTable('not.existing.repo', 'test');
+    }
+}
