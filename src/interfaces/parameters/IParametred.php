5dcac65354a8be71c06be5609947e98aa828746c
jeyroik <me@jeyroik.ru>
2023-06-26 17:11:34 +0300

    added IHasValue to params

--- src/interfaces/parameters/IParametred.php
+++ src/interfaces/parameters/IParametred.php
@@ -3,9 +3,10 @@ namespace extas\interfaces\parameters;
 
 use extas\interfaces\IHasDescription;
 use extas\interfaces\IHasName;
+use extas\interfaces\IHasValue;
 use extas\interfaces\IItem;
 
-interface IParametred extends IItem, IHasName, IHasDescription, IHaveParams
+interface IParametred extends IItem, IHasName, IHasDescription, IHaveParams, IHasValue
 {
     public const SUBJECT = 'extas.parametred';
 }
